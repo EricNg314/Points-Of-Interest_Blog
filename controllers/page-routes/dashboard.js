8c2f1fbe869d015e6116976fe7da13ba91e64d6c
Okens <Ng.Eric314@gmail.com>
2022-05-02 01:30:51 -0700

    Adding route for posts to be displayed, adding initial dashboard display with posts.

--- controllers/page-routes/dashboard.js
+++ controllers/page-routes/dashboard.js
@@ -1,9 +1,59 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', async (req, res) => {
-  res.render('dashboard', {
-    loggedIn: req.session.loggedIn
-  })
+  try {
+    const postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_comment',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+
+    // console.log('postData: ', postData)
+    // .then(dbPostData => res.json(dbPostData))
+    // .catch(err => {
+    //   console.log(err);
+    //   res.status(500).json(err);
+    // });
+
+    const posts = postData.map((post) => {
+      let data = {}
+      data = post.dataValues;
+      data.user = post.dataValues.user.dataValues;
+      return data;
+    })
+
+    console.log('posts: ', posts)
+
+
+    res.render('dashboard', {
+      posts: posts,
+      loggedIn: req.session.loggedIn
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
 })
 
 
